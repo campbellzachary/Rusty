de3154494edd72cc16a0311a9a0c8eed4e3c33b5
zacharycampbell <campbell.zachary@gmail.com>
2014-01-17 00:30:07 +0000

    Version 1.8

--- src/GameState/MenuState.java
+++ src/GameState/MenuState.java
@@ -1,18 +1,36 @@
 package GameState;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import TileMap.Background;
 
 public class MenuState extends GameState {
 	
-	private BackGround bg;
+	private Background bg;
+	private int currentChoice = 0;
+	private String[] options = {
+			"Start",
+			"Help",
+			"Quit"
+	};
+	
+	private Color titleColor;
+	private Font titleFont;
+	private Font font;
 	
 	public MenuState(GameStateManager gsm) { // Every game state needs a reference to the gamestate manager
 		
 		this.gsm = gsm;
 		
+		try {
+			bg = new Background("/Background/menubg.gift", 1);
 
 	}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		}
 
 	public void init() {}
 	public void update() {}
--- src/TileMap/Background.java
+++ src/TileMap/Background.java
@@ -1,30 +1,85 @@
 package TileMap;
 
-import java.awt.image.*;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
 import javax.imageio.ImageIO;
+
+import com.goldenharpstudios.rez.GamePanel;
 
 
 public class Background {
-	
+
 	private BufferedImage image;
-	
-	private double x; 
+	private int width; 
+	private int height; 
+
+	private double x;
 	private double y;
 	private double dx;
 	private double dy;
-	
+
 	private double moveScale;
-	
+
 	public Background(String s, double ms) {
-		
+
 		try {
 			image = ImageIO.read(
 					getClass().getResourceAsStream(s)
-		);
-			
+					);
+			width = image.getWidth(); ///////////////////
+			height = image.getHeight(); //////////////////
+			moveScale = ms;
 		}
-		catch (Exception e) {
-			
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void setPosition(double x, double y) {
+		this.x = (x * moveScale); 
+		this.y = (y * moveScale); 
+		fixPosition();
+	}
+
+	public void setVector(double dx, double dy) {
+		this.dx = dx;
+		this.dy = dy;
+	}
+
+	public void update() {
+		x += dx;
+		y += dy;
+		fixPosition(); 
+	}
+
+	private void fixPosition() {
+		while(x <= -width) x += width;
+		while(x >= width) x -= width;
+		while(y <= -height) y += height;
+		while(y >= height) y -= height;
+	}
+
+	public void draw(Graphics2D g) {
+
+		g.drawImage(image, (int)x, (int)y, null);
+
+		if(x < 0) {
+			g.drawImage(
+					image,
+					(int)x + GamePanel.WIDTH,
+					(int)y,
+					null
+					);
+		}
+		if(x > 0) {
+			g.drawImage(
+					image,
+					(int)x - GamePanel.WIDTH,
+					(int)y,
+					null
+					);
 		}
 	}
 
